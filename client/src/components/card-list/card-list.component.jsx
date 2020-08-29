e8e50304700476c66c5c7eb6a1d438b1b399cec7
Sam Barot <sbarot@inmoment.com>
2020-08-29 16:38:31 -0400

    modified cardlist and card component added null check on cardlist component

--- client/src/components/card-list/card-list.component.jsx
+++ client/src/components/card-list/card-list.component.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Card from '../card/card.component';
 
-export default ({ tweets }) => {
-    console.log(tweets);
-    return tweets
-        ? tweets.map((tweet) => <Card key={tweet.id} tweet={tweet} />)
-        : null;
-};
+export default ({ tweets }) =>
+    tweets.map((tweet) => <Card key={tweet.id} tweet={tweet} />);
--- client/src/components/search-tweets/search-tweets.component.jsx
+++ client/src/components/search-tweets/search-tweets.component.jsx
@@ -5,38 +5,36 @@ import axios from 'axios';
 import { Grid } from '@material-ui/core';
 
 export default () => {
-  const [tweets, setTweets] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
+    const [tweets, setTweets] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const { data } = await axios.get('/recentTweets', {
-        params: {
-          query: searchQuery,
-          count: 10,
-        },
-      });
-      if (data) setTweets(data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.get('/recentTweets', {
+                params: {
+                    query: searchQuery,
+                    count: 10,
+                },
+            });
+            if (data) setTweets(data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-  return (
-    <div>
-      <Grid item>
-        <form onSubmit={handleSubmit}>
-          <SearchBar
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          ></SearchBar>
-        </form>
-      </Grid>
+    return (
+        <div>
+            <Grid item>
+                <form onSubmit={handleSubmit}>
+                    <SearchBar
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    ></SearchBar>
+                </form>
+            </Grid>
 
-      <Grid item>
-        <TweetList tweets={tweets} />
-      </Grid>
-    </div>
-  );
+            <Grid item>{tweets ? <TweetList tweets={tweets} /> : null}</Grid>
+        </div>
+    );
 };
