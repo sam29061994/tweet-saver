22e1f3d807bf532efd9700378b6d9ee7c233905d
Sam Barot <sbarot@inmoment.com>
2020-08-29 14:50:28 -0400

    Removed extra logs

--- client/src/components/card-list/card-list.component.jsx
+++ client/src/components/card-list/card-list.component.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Card from '../card/card.component';
 
-export default ({ tweets }) => {
-    console.log(JSON.stringify(tweets));
-    return tweets
+export default ({ tweets }) =>
+    tweets
         ? tweets.map((tweet) => {
               console.log(tweet);
               return <Card key={tweet.id} tweet={tweet} />;
           })
         : null;
-};
