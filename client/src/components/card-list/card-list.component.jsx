b6d5c4671849f8a5026646c4c19eefc21b7bab7d
Sam Barot <sbarot@inmoment.com>
2020-08-29 16:25:26 -0400

    .

--- client/src/components/card-list/card-list.component.jsx
+++ client/src/components/card-list/card-list.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Card from '../card/card.component';
 
-export default ({ tweets }) =>
-    tweets
+export default ({ tweets }) => {
+    console.log(tweets);
+    return tweets
         ? tweets.map((tweet) => <Card key={tweet.id} tweet={tweet} />)
         : null;
+};
