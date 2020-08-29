d16e11b757cf59881e1ffaf3188591c61755ca0b
Sam Barot <sbarot@inmoment.com>
2020-08-29 15:39:35 -0400

    added dependency for express-sslify

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require('express-sslify');
 const Twit = require('twit');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
